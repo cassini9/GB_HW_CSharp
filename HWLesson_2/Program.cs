6d356bd12dc4df9fa1ea42a7094d930ebbfc2cf0
Fedor <fedormaks@gmail.com>
2022-06-21 16:11:22 +0300

    second task completed

--- HWLesson_2/Program.cs
+++ HWLesson_2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 
-// void ShowNumber() {
+// void ShowNumber1() {
 //     Console.Clear();
 
 //     Console.WriteLine("Enter 3 digits number:");
@@ -14,13 +14,23 @@
     
 // }
 
-// ShowNumber();
+// ShowNumber1();
 
 
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+// void ThirdNumber(int a){
+    
+//     if(a/10 > 9){
+//         Console.WriteLine(a%10 + " third digit");
+//     }else{
+//         Console.WriteLine("no third digit exists");
+//     }
+// }
 
-
+// Console.Clear();
+// Console.WriteLine("Enter 1, 2 or 3 digits number:");
+// ThirdNumber(Convert.ToInt32(Console.ReadLine()));
 
 
 // Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
