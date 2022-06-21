0318186508c2af91a5be591c4775faf7bc536cdd
Fedor <fedormaks@gmail.com>
2022-06-21 15:00:19 +0300

    First task completed

--- HWLesson_2/Program.cs
+++ HWLesson_2/Program.cs
@@ -1,7 +1,20 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 
+// void ShowNumber() {
+//     Console.Clear();
 
+//     Console.WriteLine("Enter 3 digits number:");
+//     int number = Convert.ToInt32(Console.ReadLine());
+
+//     number = number/10;
+//     number = number%10;
+
+//     Console.WriteLine("Second digit" + "->" + number);
+    
+// }
+
+// ShowNumber();
 
 
 // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
