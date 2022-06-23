150a4674eb3fc7bfddde47a709774ed98f0c5d20
Fedor <fedormaks@gmail.com>
2022-06-23 20:28:11 +0300

    First task completed

--- HWLesson_4/Program.cs
+++ HWLesson_4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+double PowerAB(int a, int b) {
+    return Math.Pow((a), b);
+}
+
+Console.WriteLine("Enter two numbers one by one:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Result is " + PowerAB(a, b));
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+
+//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+
