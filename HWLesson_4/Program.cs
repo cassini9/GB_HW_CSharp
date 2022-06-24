2f60287a1f6d84882584b0dfadd1d0c2e5280f36
Fedor <fedormaks@gmail.com>
2022-06-24 10:03:26 +0300

    1,2 tasks completed

--- HWLesson_4/Program.cs
+++ HWLesson_4/Program.cs
@@ -1,17 +1,33 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-double PowerAB(int a, int b) {
-    return Math.Pow((a), b);
-}
+// double PowerAB(int a, int b) {
+//     return Math.Pow((a), b);
+// }
 
-Console.WriteLine("Enter two numbers one by one:");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter two numbers one by one:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
 
-Console.WriteLine("Result is " + PowerAB(a, b));
+// Console.WriteLine("Result is " + PowerAB(a, b));
 
 //Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+// int SumNum(int num){
+
+//     int sum = 0;
+
+//     for (int i = 0; i <= sum; i++){
+        
+//             sum = sum + num%10;
+//             num = num/10;
+        
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Enter the number:");
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("The result of digit's sum in the number is " + SumNum(number));
 
 
 //Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
