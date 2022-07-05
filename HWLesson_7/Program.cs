fc07bc2547d098ae0bcfa4796a99c22186ac825e
Fedor <fedormaks@gmail.com>
2022-07-05 17:43:01 +0300

    Second task completed

--- HWLesson_7/Program.cs
+++ HWLesson_7/Program.cs
@@ -39,6 +39,65 @@
 //и возвращает значение этого элемента или же указание, что такого элемента нет.
 
 
+// int[,] CreateRandomArray(int m, int n, int min, int max)
+// {
+//     int[,] array = new int[m, n];
+//     for (int i = 0; i < m; i++)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             array[i, j] = new Random().Next(min, max);
+//         }
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[,] array)
+// {
+
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+
+// }
+
+// int[,] myArray = CreateRandomArray(4, 3, 1, 20);
+
+
+// void CheckArrayElenentByPosition(int m, int n, int[,] array)
+// {
+//     if (m < array.GetLength(0) && n < array.GetLength(1))
+//     {
+//         for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
+//                 if (i == m && j == n)
+//                 {
+//                     Console.WriteLine("The array's element: " + array[i, j]);
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     else {
+//         Console.WriteLine("No elements with such a position exist");}
+// }
+
+
+// ShowArray(myArray);
+
+// Console.WriteLine("Enter row position: ");
+// int row = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Enter column position: ");
+// int column = Convert.ToInt32(Console.ReadLine());
+
+// CheckArrayElenentByPosition(row, column, myArray);
 
 
 
