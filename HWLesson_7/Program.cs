529a47a0dcb6e5e66b15160acbfb4441fd0ab654
Fedor <fedormaks@gmail.com>
2022-07-05 19:46:59 +0300

    All tasks completed

--- HWLesson_7/Program.cs
+++ HWLesson_7/Program.cs
@@ -104,4 +104,55 @@
 //Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце
 
 
+int[,] CreateRandomArray(int m, int n, int min, int max)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i, j] = new Random().Next(min, max);
+        }
+    }
+    return array;
+}
+
+void ShowArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] myArray = CreateRandomArray(3, 4, 1, 11);
+
+ShowArray(myArray);
+
+void MeanForColumns(int[,] array)
+{
+    double result = 0;
+    double sum = 0;
+
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+                sum = sum + array[i,j];
+        }
+        result = sum/array.GetLength(0);
+        Console.WriteLine("Mean for the column is " + Math.Round(result, 2) + " correspondly");
+        sum = 0;
+    }
+}
+
+
+MeanForColumns(myArray);
+
 
